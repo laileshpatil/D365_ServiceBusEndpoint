1d1f075edd330bbe72b4ae68403cbbe103a63609
laileshpatil <52512925+laileshpatil@users.noreply.github.com>
2024-03-04 05:27:53 +0000

    Update D365ServiceEndpointServiceBus.psm1
    udpate

--- D365ServiceEndpointServiceBus.psm1
+++ D365ServiceEndpointServiceBus.psm1
@@ -75,10 +75,9 @@ function Add-TopicServiceEndpoint
    
     $existing = $null
  
+    
     Write-Host "Checking for existing service endpoint for Topic $TopicName, ServiceEndpoint $ServiceEndpointName" -ForegroundColor Green
-    if ($ServiceBusName) {
-        $existing = Get-ServiceEndpoint -Connection $Connection -ServiceEndpointName $ServiceEndpointName
-    }
+    $existing = Get-ServiceEndpoint -Connection $Connection -ServiceEndpointName $ServiceEndpointName
  
     $endpoint = @{ }
     $endpoint.Add("name", $ServiceEndpointName)
@@ -96,12 +95,15 @@ function Add-TopicServiceEndpoint
     if ($existing) {
         Write-Host "Existing service endpoint" -ForegroundColor Green
         #Set-CrmRecord -conn $Connection -Fields $endpoint -Id $existing.serviceendpointid -EntityLogicalName "serviceendpoint"
+        return $existing.serviceendpointid
     } else {
         Write-Host "Creating service endpoint" -ForegroundColor Green
         #$endpoint.Add("serviceendpointid", $ServiceEndpointId)
         $id = New-CrmRecord -conn $Connection -Fields $endpoint -EntityLogicalName "serviceendpoint"
        
         Write-Host "Created with ServiceEndpointId = $id"
+
+        return $id
     }
 }
  
@@ -125,18 +127,61 @@ function Remove-ServiceEndpoint
 }
 
 
+function Add-EntitySdkMessageProcessingStepImage(
+    [Microsoft.Xrm.Tooling.Connector.CrmServiceClient] $Connection,
+    [guid] $Sdkmessageprocessingstepid, 
+    [string] $Message, 
+    [string] $EntityLogicalName,
+    [string] $Name
+) 
+{
+    Write-Host "Processing Image for sdkmessageprocessingstepid " $Sdkmessageprocessingstepid
+    $fields= @{}
+    
+    $fields.Add("messagepropertyname", "Target")
+    $fields.Add("name", $Name)
+
+    switch ($Message.ToLower()) {
+        'create' {  
+            $fields.Add("imagetype", (New-CrmOptionSetValue 1))
+        }
+        'update'{
+            $fields.Add("imagetype", (New-CrmOptionSetValue 2))
+        }
+        Default {
+            $fields.Add("imagetype", (New-CrmOptionSetValue 0))
+        }
+    }
+
+    $fields.Add("entityalias", $EntityLogicalName)
+    $fields.add("sdkmessageprocessingstepid", [Microsoft.Xrm.Sdk.EntityReference]::new("sdkmessageprocessingstep", $Sdkmessageprocessingstepid))
+
+    $existing = Get-SdkMessageProcessingStepImage -Connection $Connection -Sdkmessageprocessingstepid $Sdkmessageprocessingstepid -Name $name
+
+    $imageId = $null
+
+    if ($existing) {
+        Write-Host "Existing sdkmessageprocessingstepimage" -ForegroundColor Green
+        $imageId = $existing.sdkmessageprocessingstepimageId
+        #Set-CrmRecord -conn $Connection -Fields $endpoint -Id $fields.sdkmessageprocessingstepId -EntityLogicalName "sdkmessageprocessingstep"
+    } else {
+        Write-Host "Creating New sdkmessageprocessingstepimage " -ForegroundColor Green
+        $imageId = New-CrmRecord -conn $Connection -Fields $fields -EntityLogicalName "sdkmessageprocessingstepimage"
+       
+        Write-Host "Created New sdkmessageprocessingstepimageId $imageId"
+    }
+
+    Write-host "**sdkmessageprocessingstepimageId**" $imageId
+    
+}
+
+
 function Add-EntitySdkMessageProcessingStep
 (
     [GUID] $ServiceEndpointId,
-    [Microsoft] $MessageId,
-    [string] $MessageName,
+    [string] $Message,
     [string] $EntityLogicalName,
-    [string] $ServiceName,
     [string] $ServiceBusName,
-    [string] $ServiceBusUrl,
-    [string] $SasKeyName,
-    [string] $SasKey,
-    [string] $TopicName,
     [Microsoft.Xrm.Tooling.Connector.CrmServiceClient] $Connection
 ) {
    
@@ -159,63 +204,108 @@ function Add-EntitySdkMessageProcessingStep
 
     #>
    
- 
+    
+    Write-Host "getting sdkmessageid for " $Message 
     $sdkmessageid = Get-SdkMessage -Connection $Connection -Message $Message 
 
+    if($sdkmessageid)
+    {
+        Write-Host "sdkmessageid found " $sdkmessageid
+    }else {
+        throw "sdkMessageid not found." 
+    }
+
+    Write-Host "geting sdkmessagefilterid for sdkmessageid " $sdkmessageid " Entity " $EntityLogicalName
     $sdkmessagefilterid = Get-SdkMessageFilterRef -Connection $Connection -MessageId $sdkmessageid -EntityLogicalName $EntityLogicalName
 
-   
+    if($sdkmessagefilterid)
+    {
+        Write-Host "sdkmessagefilterid found" $sdkmessagefilterid
+    }else {
+        throw "sdkmessagefilterid not found." 
+    }
+
+    $name = ("{0} {1}: {2} of {0}" -f $EntityLogicalName, $ServiceBusName, $Message)
     $existing = $null
-   
+
+    Write-Host "Checking existing Message Processing Step " $name 
+
+    $existing = Get-EntitySdkMessageProcessingStep -Connection $Connection -sdkmessageid $sdkmessageid -sdkmessagefilterid $sdkmessagefilterid -stepName $name
+
+    Write-Host "Processing for Step " $name
     $fields= @{}
-    $fields.Add("name", "Event Processing after $MessageName for Entity $EntityLogicalName")
+ 
+    $fields.Add("name", $name)
+
     $fields.Add("asyncautodelete", $true) #asynautodelete 1: Yes, 0: No
-    $fields.add("eventhandler", $ServiceEndpointId) #serviceEndpoint reference
+    $fields.add("eventhandler", [Microsoft.Xrm.Sdk.EntityReference]::new("serviceendpoint", $ServiceEndpointId)) #serviceEndpoint reference
     $fields.Add("invocationsource", (New-CrmOptionSetValue 0)) #Run in user Context -1:Internal, 0:Parent, 1:Child
     $fields.Add("iscustomizable", $true) #iscustomizable 1: Yes, 0: No
     $fields.Add("ishidden", $false) #ishidden 1: Yes, 0: No
     $fields.Add("mode", (New-CrmOptionSetValue 1)) #Execution mode 1:asyn , 0:syn
     $fields.Add("rank", 1) #Execution Order
     #sdkmessage
-    $fields.Add("sdkmessage", $sdkmessageid) # Message Id
-    $fields.Add("sdkmessagefilterid", $sdkmessagefilterid)
+    $fields.Add("sdkmessageid", [Microsoft.Xrm.Sdk.EntityReference]::new("sdkmessage", $sdkmessageid)) # Message Id
+    $fields.Add("sdkmessagefilterid", [Microsoft.Xrm.Sdk.EntityReference]::new("sdkmessagefilter", $sdkmessagefilterid) )
     $fields.Add("stage", (New-CrmOptionSetValue 40)) #State: 40 - Post operation
     $fields.Add("statecode", (New-CrmOptionSetValue 0)) #0: Enabled, 1:Disabled
     $fields.Add("supporteddeployment", (New-CrmOptionSetValue 0)) #Deployment 0: Server, 1 D365 client for Outlook, 2: Both
 
+    $id = $null
 
     if ($existing) {
-        Write-Host "Updating service endpoint" -ForegroundColor Green
-        Set-CrmRecord -conn $Connection -Fields $endpoint -Id $existing.serviceendpointid -EntityLogicalName "serviceendpoint"
+        $id = $existing.sdkMessageProcessingStepId
+        Write-Host "Existing sdkmessageprocessingstep " $id -ForegroundColor Green
+        #Set-CrmRecord -conn $Connection -Fields $endpoint -Id $fields.sdkmessageprocessingstepId -EntityLogicalName "sdkmessageprocessingstep"
     } else {
-        Write-Host "Creating service endpoint" -ForegroundColor Green
-        $endpoint.Add("serviceendpointid", $ServiceEndpointId)
-        $id = New-CrmRecord -conn $Connection -Fields $endpoint -EntityLogicalName "serviceendpoint"
+        Write-Host "Creating New sdkmessageprocessingstep " -ForegroundColor Green
+        $id = New-CrmRecord -conn $Connection -Fields $fields -EntityLogicalName "sdkmessageprocessingstep"
        
-        Write-Host "Created with ServiceEndpointId = $id"
+        Write-Host "Created with sdkmessageprocessingstepId = $id"
     }
+
+    #Adding SdkMessageProcessingStepImage
+    if ($id)
+    {
+        $imageName = ("Image for {0} post {1}" -f $EntityLogicalName, $Message)
+        Add-EntitySdkMessageProcessingStepImage -Connection $Connection -Sdkmessageprocessingstepid $id -Message $Message -EntityLogicalName $EntityLogicalName -Name $imageName
+    }
+
+    return $id
  
     #SdkMessageProcessingStepId
 }
  
 function Get-EntitySdkMessageProcessingStep
 (
-    [GUID] $EntityId
+    [guid] $sdkmessageid, 
+    [guid] $sdkmessagefilterid, 
+    [string] $stepName,
+    [Microsoft.Xrm.Tooling.Connector.CrmServiceClient] $Connection
 )
 {
-   
-}
- 
+      
+    $fetchXml =
+@"
+<fetch no-lock="true">
+<entity name="sdkmessageprocessingstep">
+<filter>
+    <condition attribute="sdkmessageid" operator="eq" value="$sdkmessageid" />
+    <condition attribute="sdkmessagefilterid" operator="eq" value="$sdkmessagefilterid" />
+    <condition attribute="name" operator="eq" value="$stepName" />
+    
+</filter>  
+</entity>
+</fetch>
+"@
 
+Write-Host @fetchXml
+$result = Get-CrmRecordsByFetch -conn $Connection -Fetch $fetchXml -WarningAction SilentlyContinue
 
+Write-Host $result.CrmRecords[0]
 
-function Get-EntitySdkMessage
-(
-    [string] $EntityName,
-    [string] $MessageType
-)
-{
-    return [GUID]
+Return $result.CrmRecords[0]
+
 }
  
 
@@ -225,6 +315,8 @@ function Get-SdkMessage
     [Microsoft.Xrm.Tooling.Connector.CrmServiceClient] $Connection
 ) 
 {
+
+    Write-Host "Getting sdkMessage for " $Message
 
     $fetchXml =
     @"
@@ -238,12 +330,10 @@ function Get-SdkMessage
 </fetch>
 "@
 
-    Write-Host "Getting sdkMessage for " $Message
+    
     Write-Host $fetchXml
 
     $result = Get-CrmRecordsByFetch -conn $Connection -Fetch $fetchXml -WarningAction SilentlyContinue
-    Write-Host $result
-
     return $result.CrmRecords[0].sdkmessageid
  
 }
@@ -277,5 +367,44 @@ function Get-SdkMessageFilterRef
  
     Write-Host $result
     return $result.CrmRecords[0].sdkmessagefilterid
+
+}
+
+function Get-SdkMessageProcessingStepImage 
+(
+    [guid]  $SdkMessageProcessingStepId,  
+    [string] $Name,
+    [Microsoft.Xrm.Tooling.Connector.CrmServiceClient] $Connection
+) 
+{
+
+    Write-Host "Retrive SdkMessageProcessingStepImage for sdkmessageprocessingstepid " $Sdkmessageprocessingstepid " Name " $Name
+ 
+    $fetchXml =
+    @"
+    <fetch no-lock="true">
+    <entity name="sdkmessageprocessingstepimage">
+        <filter>
+            <condition attribute="sdkmessageprocessingstepid" operator="eq" value="$Sdkmessageprocessingstepid" />
+            <condition attribute="name" operator="eq" value="$Name" />
+        </filter>
+    </entity>
+    </fetch>
+"@
+
+    Write-Host "Query: " $fetchXml
+    
+    $result = Get-CrmRecordsByFetch -conn $Connection -Fetch $fetchXml -WarningAction SilentlyContinue
+
+    Write-Host "Query Result Count: " $result.CrmRecords.Count
+    #Write-Host $result.CrmRecords[0]
+
+    $response = $null
+
+    if ($result.CrmRecords.Count -gt 0) {
+        $response = $response.CrmRecords
+    }    
+
+    return $respons
 }
 
